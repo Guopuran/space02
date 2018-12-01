766867a6c90a2f3175291da9e1ae1c62fc67e2be
guopuran <www.1174965274@qq.com>
2018-12-01 11:58:00 +0800

    this is ViewGroup 流式布局+attrs.xml+自定义view

--- app7/src/main/java/guopuran/bwie/com/app7/CustomView.java
+++ app7/src/main/java/guopuran/bwie/com/app7/CustomView.java
@@ -9,8 +9,13 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class CustomView extends LinearLayout {
+    //孩子中最高的一个
     private int mChildMaxHeight;
+    //每一行的上下的间距
+    //20是默认值，单位是px
     private int mVSpace=20;
+    //每一个孩子的左右的间距
+    //20是默认值，单位是px
     private int mHSpace=20;
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -19,9 +24,10 @@ public class CustomView extends LinearLayout {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //拿到父容器推荐的宽和高以及计算模式
         int sizewidth=MeasureSpec.getSize(widthMeasureSpec);
         int sizeheight=MeasureSpec.getSize(heightMeasureSpec);
-        //测量孩子的大小
+        //测量孩子的大小，一定要写这个
 //        setMeasuredDimension(widthMeasureSpec,heightMeasureSpec);
 
         measureChildren(widthMeasureSpec,heightMeasureSpec);
@@ -31,10 +37,12 @@ public class CustomView extends LinearLayout {
         int left=0,top=0;
         //循环所有的孩子
         int childcount=getChildCount();
-        Log.i("dj", "---------------------------");
+
         for (int i=0;i<childcount;i++){
             View view=getChildAt(i);
+            //是否是一行的开头
             if (left!=0){
+                //需要换行了，因为放不下啦
                 if (left+view.getMeasuredWidth()>sizewidth){
                     //计算出下一行的高度
                     top+=mChildMaxHeight+mVSpace;
@@ -42,12 +50,10 @@ public class CustomView extends LinearLayout {
                 }
             }
             left+=view.getMeasuredWidth()+mHSpace;
-            Log.i("dj",  i + " mChildMaxHeight is " + mChildMaxHeight);
-            Log.i("dj", i + " MeasuredWidth is " + view.getMeasuredWidth());
+
         }
 
-        Log.i("dj", "top is " + top);
-        Log.i("dj", "left is " + left);
+
         setMeasuredDimension(sizewidth,(top+mChildMaxHeight)>sizeheight?sizeheight:top+mChildMaxHeight);
     }
 
@@ -55,7 +61,11 @@ public class CustomView extends LinearLayout {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
         findMaxChildHeight();
+        //开始安排孩子的位置
+
+        //初始化值
         int left=0,top=0;
+        //循环所有的孩子
         int childcount=getChildCount();
         for (int i=0;i<childcount;i++){
             View view=getChildAt(i);
@@ -76,7 +86,7 @@ public class CustomView extends LinearLayout {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
     }
-
+    //寻找孩子中最高的一个孩子
     private void findMaxChildHeight() {
         mChildMaxHeight=0;
         int childcount=getChildCount();
